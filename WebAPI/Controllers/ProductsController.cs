b9dda99e28519b32286b57fdbc59de7633f9aa1a
Furkan Kavak <furkankvk1621@gmail.com>
2021-02-19 23:37:17 +0300

    19.02.2021

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entites.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Losely coupled
+        //naming convetion
+        //IoC Container->İnversion of control;değişimin kontrolü 
+
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult GetAll()
+        {
+            //return IProductServiceTest();
+
+            //Swagger
+            //Dependency Chain ---
+
+            var result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetId(int id)
+        {
+            var result = _productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+
+       
+
+
+
+        private static List<Product> IProductServiceTest()
+        {
+            //Dependnecy chain--Bağımlılık zinciiri nasıl diyeceksen;productService->ProductManager->EfProductDal(), zincir böyle 
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+        }
+    }
+}
